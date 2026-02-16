49fbef2a057575ad5242b8072b43b79f762b877b
Hans Flächsig <hans@flaechsig.de>
2026-02-16 21:04:26 +0100

    Tests wieder lauffähig gemacht

--- blocpress-workbench/src/test/java/io/github/flaechsig/blocpress/workbench/WorkbenchHealthIT.java
+++ blocpress-workbench/src/test/java/io/github/flaechsig/blocpress/workbench/WorkbenchHealthIT.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -24,11 +26,27 @@ class WorkbenchHealthIT {
     private static final Logger LOG = LoggerFactory.getLogger(WorkbenchHealthIT.class);
     private static final String IMAGE = System.getProperty("it.image");
 
+    static final Network network = Network.newNetwork();
+
+    @Container
+    static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:17"))
+                    .withDatabaseName("workbench")
+                    .withUsername("workbench")
+                    .withPassword("workbench")
+                    .withNetwork(network)
+                    .withNetworkAliases("postgres");
+
     @Container
     static final GenericContainer<?> app =
             new GenericContainer<>(DockerImageName.parse(IMAGE))
                     .withExposedPorts(8081)
+                    .withNetwork(network)
+                    .withEnv("QUARKUS_DATASOURCE_JDBC_URL", "jdbc:postgresql://postgres:5432/workbench")
+                    .withEnv("QUARKUS_DATASOURCE_USERNAME", "workbench")
+                    .withEnv("QUARKUS_DATASOURCE_PASSWORD", "workbench")
                     .withLogConsumer(new Slf4jLogConsumer(LOG).withPrefix("workbench"))
+                    .dependsOn(postgres)
                     .waitingFor(
                             Wait.forHttp("/q/health/ready")
                                     .forPort(8081)
